322b1e4fe971d2d054b86dcccb395a21c63717e5
Jon Bertelsen <jobe@cphbusiness.dk>
2021-02-04 11:57:17 +0100

    Add User Class

---
+++ src/main/java/User.java
@@ -0,0 +1,26 @@
+public class User {
+
+    private String userName;
+    private String password;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
